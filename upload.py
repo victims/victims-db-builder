21221fa7dfc67f076202afe243927502e5b2470e
Jason Shepherd <jshepherd@redhat.com>
2015-09-23 16:14:57 +1000

    altered uploadArchive to pass file contents in body

--- upload.py
+++ upload.py
@@ -1,4 +1,4 @@
-import requests, string, base64, hash 
+import requests, string, base64, hash
 
 filename="/Users/jasonshepherd/Downloads/spring-web-4.2.0.RELEASE.jar"
 hostname="localhost"
@@ -6,14 +6,18 @@ port=5000
 username="jshepher"
 password="Welcome1!"
 
-def uploadArchive(filename, version, groupId, artifactId, cves):
-	file = open(filename)
-	files = { 'file': file } 
-	print "uploading file: " + filename
-        server = "http://{0}:{1}".format(hostname, port)
-        url = "{0}/service/submit/archive/java/?version={1}&groupId={2}&artifactId={3}&cves={4}".format(server, version, groupId, artifactId, cves)
-	print url 
-	response = requests.put(url, files, auth=(username, password))
+def uploadArchive(filename, cves):
+	archive = open(filename)
+	#files = { 'file': file }
+	#print "uploading file: " + filename
+	server = "http://{0}:{1}".format(hostname, port)
+	url = "{0}/service/v2/submit/archive/java/?cves={1}".format(server, cves)
+	print "url:" + url
+	response = requests.put(url,
+		data={
+			'archive' : archive
+		},
+		auth=(username, password))
 	print response
 
 def uploadHash(filename):
@@ -23,23 +27,23 @@ def uploadHash(filename):
 		'artifactId':'spring-web',
 		'version':'4.2.0'
 	}
-	print coordinates
-	
-	requestDict = { 
+	#print coordinates
+
+	requestDict = {
 		'entry':fullHash,
 		'cves': ['CVE-2015-0000'],
 		'coordinates':coordinates
 	}
 
-	print requestDict
+	#print requestDict
 
 #import urllib2, base64
 
 #request = urllib2.Request(URL)
 #base64string = base64.encodestring('%s:%s' % (KEY, PASSWORD)).replace('\n', '')
-#request.add_header("Authorization", "Basic %s" % base64string)   
+#request.add_header("Authorization", "Basic %s" % base64string)
 #result = urllib2.urlopen(request)
 
-#upload(filename,"4.2.0", "org.springframework", "spring-web", "CVE-2013-0000")
+uploadArchive(filename, "CVE-2013-0000")
 
-uploadHash(filename)
+#uploadHash(filename)
