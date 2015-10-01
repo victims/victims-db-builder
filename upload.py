de0af0ff55949a5524d1ee79733722e6c0252771
Jason Shepherd <jshepherd@redhat.com>
2015-10-01 10:54:58 +1000

    got upload working

--- upload.py
+++ upload.py
@@ -1,47 +1,48 @@
-import requests, string, base64, hash
+import requests
+from hmac import HMAC
+from datetime import datetime
+from hashlib import md5, sha512
 
 filename="/Users/jasonshepherd/Downloads/spring-web-4.2.0.RELEASE.jar"
 hostname="localhost"
-port=5000
-username="jshepher"
-password="Welcome1!"
+port=8080
+secret = "CAB87262DF0B02D4C5D79D64B0E80D91A9539B24"
+apiKey = "00F25937A918DB067428B1CB5F5AC148"
 
-def uploadArchive(filename, cves):
-	#files = { 'file': file }
-	#print "uploading file: " + filename
-	server = "http://{0}:{1}".format(hostname, port)
-	url = "{0}/service/v2/submit/archive/java/?cves={1}".format(server, cves)
-	print "url:" + url
-	with open(filename, 'rb') as archive:
-		response = requests.put(url,
-			data=archive,
-			auth=(username, password))
-	#print response
+def generate_signature(secret, method, path, date, md5sums):
+    md5sums.sort()
+    ordered = [method, path, date] + md5sums
+    string = ''
 
-def uploadHash(filename):
-	fullHash = hash.doHash(filename)
-	coordinates = {
-		'groupId':'org.springframework',
-		'artifactId':'spring-web',
-		'version':'4.2.0'
-	}
-	#print coordinates
+    for content in ordered:
+        if content is None:
+            raise ValueError('Required header not found')
+        string += str(content)
 
-	requestDict = {
-		'entry':fullHash,
-		'cves': ['CVE-2015-0000'],
-		'coordinates':coordinates
-	}
+    return HMAC(
+        key=bytes(secret),
+        msg=string.lower(),
+        digestmod=sha512
+    ).hexdigest().upper()
 
-	#print requestDict
+def uploadArchive(filename, gid, aid, vid, cves):
+    print "uploading file: " + filename
+    server = "http://%s:%s" % (hostname, port)
+    path = "/service/v2/submit/archive/java/?version=%s\&groupId=%s\&artifactId=%s\&cves=%s" % (
+        vid, gid, aid, ','.join(cves))
+    url = server + path
+    date = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+    with open(filename, 'rb') as archive:
+        md5sums = [md5(archive.read()).hexdigest()]
+        archive.seek(0)
+        files = { 'archive': archive }
+        response = requests.put(url,
+            headers = {'X-Victims-Api': "%s:%s" % (apiKey, generate_signature(
+            	secret, 'PUT', path, date, md5sums)),
+            	"Date": date},
+            files=files
+            )
+    print response.text
 
-#import urllib2, base64
 
-#request = urllib2.Request(URL)
-#base64string = base64.encodestring('%s:%s' % (KEY, PASSWORD)).replace('\n', '')
-#request.add_header("Authorization", "Basic %s" % base64string)
-#result = urllib2.urlopen(request)
-
-uploadArchive(filename, "CVE-2013-0002")
-
-#uploadHash(filename)
+uploadArchive(filename, 'org.springframework', 'spring-web', '4.2.0', ["CVE-2013-0004"])
