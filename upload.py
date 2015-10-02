2cbf2b02dc1ae5f19e32ab2da31b35802f356657
Jason Shepherd <jshepherd@redhat.com>
2015-10-02 12:15:26 +1000

    switched backed to basic auth

--- upload.py
+++ upload.py
@@ -5,9 +5,11 @@ from hashlib import md5, sha512
 
 filename="/Users/jasonshepherd/Downloads/spring-web-4.2.0.RELEASE.jar"
 hostname="localhost"
-port=8080
-secret = "CAB87262DF0B02D4C5D79D64B0E80D91A9539B24"
-apiKey = "00F25937A918DB067428B1CB5F5AC148"
+port=5000
+#secret = "CAB87262DF0B02D4C5D79D64B0E80D91A9539B24"
+#apiKey = "00F25937A918DB067428B1CB5F5AC148"
+username = 'jasinner'
+password = 'Welcome1!'
 
 def generate_signature(secret, method, path, date, md5sums):
     md5sums.sort()
@@ -31,18 +33,19 @@ def uploadArchive(filename, gid, aid, vid, cves):
     path = "/service/v2/submit/archive/java/?version=%s\&groupId=%s\&artifactId=%s\&cves=%s" % (
         vid, gid, aid, ','.join(cves))
     url = server + path
-    date = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+    #date = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
     with open(filename, 'rb') as archive:
-        md5sums = [md5(archive.read()).hexdigest()]
-        archive.seek(0)
+        #md5sums = [md5(archive.read()).hexdigest()]
+        #archive.seek(0)
         files = { 'archive': archive }
         response = requests.put(url,
-            headers = {'X-Victims-Api': "%s:%s" % (apiKey, generate_signature(
-            	secret, 'PUT', path, date, md5sums)),
-            	"Date": date},
-            files=files
+            #headers = {'X-Victims-Api': "%s:%s" % (apiKey, generate_signature(
+            #	secret, 'PUT', path, date, md5sums)),
+            #	"Date": date},
+            files=files,
+            auth = (username, password)
             )
     print response.text
 
 
-uploadArchive(filename, 'org.springframework', 'spring-web', '4.2.0', ["CVE-2013-0004"])
+uploadArchive(filename, 'org.springframework', 'spring-web', '4.2.0', ["CVE-2013-0007"])
