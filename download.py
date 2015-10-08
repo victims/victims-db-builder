435ab3eeb561f7a8cdcd080d8aee031fc78368ed
Jason Shepherd <jshepherd@redhat.com>
2015-10-08 16:01:43 +1000

    need to fix up download url

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
-import vulnerability
+from vulnerability import Vulnerability
 import string
 import requests
 from os import path
@@ -7,49 +7,57 @@ from os import path
 ## Download
 ############################################################
 # http://central.maven.org/maven2/org/springframework/spring-web/4.2.0.RELEASE/spring-web-4.2.0.RELEASE.jar
-indexBaseUrl = "http://central.maven.org/maven2/"
-downloadDir = 'downloads/'
 
-def parseGroupId(groupId):
-    return string.replace(groupId, '.', '/')
+class MavenDownloader:
+    indexBaseUrl = "http://central.maven.org/maven2/"
+    downloadDir = 'downloads/'
 
-def parseVersionString(versionString):
-    return string.replace(versionString, '/', '-')
+    def __init__(self, document):
+        self.vulnerability = Vulnerability(document)
 
-def buildUrl(groupId, versionString):
-    jarName = parseVersionString(versionString) + '.jar'
-    url = '%s%s/%s/%s' % (indexBaseUrl, parseGroupId(groupId),
-        versionString, jarName)
-    return (url, jarName)
+    def parseGroupId(self, groupId):
+        return string.replace(groupId, '.', '/')
 
-def dorequest(filename, url):
-    with open(filename, 'wb') as handle:
-        response = requests.get(url, stream="True")
-        if not response.ok:
-            print "Request to download %s failed. %s: %s" % (
-                url, response.status_code, response.text)
-        else:
-            for block in response.iter_content(1024):
-                handle.write(block)
+    def parseVersionString(self, versionString):
+        return string.replace(versionString, '/', '-')
 
+    def buildUrl(self, groupId, versionString):
+        jarName = self.parseVersionString(versionString) + '.jar'
+        url = '%s%s/%s/%s' % (self.indexBaseUrl, self.parseGroupId(groupId),
+            versionString, jarName)
+        return (url, jarName)
 
-##http://central.maven.org/maven2/org/springframework/spring-web/4.1.6.RELEASE/spring-web-4.1.6.RELEASE.jar
-##############################################################
-## Parse and build
-##############################################################
-
-def download(yamlFile):
-    jars = vulnerability.Vulnerability(yamlFile)
-    #jars.print_flaw()
-    listVers = jars.checkMvnVer()
-    if listVers:
-       	for v in listVers:
-            jarUrl, jarName = buildUrl(jars.groupId, v)
-            localPath = downloadDir + jarName
-            if not path.isfile(localPath):
-                print "Downloading: %s to %s." % (jarUrl, localPath)
-                dorequest(localPath, jarUrl)
+    def dorequest(self, filename, url):
+        with open(filename, 'wb') as handle:
+            response = requests.get(url, stream="True")
+            if not response.ok:
+                print "Request to download %s failed. %s: %s" % (
+                    url, response.status_code, response.text)
             else:
-                print "%s exists." % localPath
+                for block in response.iter_content(1024):
+                    handle.write(block)
 
-download('3192.yaml')
+    def prepare_request(self, groupId, version):
+        jarUrl, jarName = self.buildUrl(groupId, version)
+        localPath = self.downloadDir + jarName
+        if not path.isfile(localPath):
+            print "Downloading: %s to %s." % (jarUrl, localPath)
+            self.dorequest(localPath, jarUrl)
+            return localPath
+        else:
+            print "%s exists." % localPath
+            return None
+
+    ##http://central.maven.org/maven2/org/springframework/spring-web/4.1.6.RELEASE/spring-web-4.1.6.RELEASE.jar
+    ##############################################################
+    ## Parse and build
+    ##############################################################
+    def download(self):
+        newfiles = []
+        listVers = self.vulnerability.checkMvnVer()
+        if listVers:
+       	    for v in listVers:
+                newLocalPath = self.prepare_request(self.vulnerability.groupId, v)
+                if newLocalPath is not None:
+                    newfiles.append((newLocalPath, v))
+        return newfiles
