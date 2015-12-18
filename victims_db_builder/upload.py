bc51f3a056ac3afc6186ea5048c9c922cdd6ee49
Jason Shepherd <jshepherd@redhat.com>
2015-12-18 16:57:41 +1000

    added support for coord only upload

--- victims_db_builder/upload.py
+++ victims_db_builder/upload.py
@@ -12,26 +12,29 @@ port = config.get('victims_api', 'port')
 protocol = config.get('victims_api', 'protocol')
 server = "{0}://{1}:{2}".format(protocol, hostname, port)
 
-
-
 def uploadArchive(username, password, filename, gid, aid, vid, cves):
-    logger.info("uploading file: " + filename)
-    server = "%s://%s:%s" % (protocol, hostname, port)
+    logger.info("uploading file: %s" % filename)
     path = "/service/v2/submit/archive/java/?version=%s\&groupId=%s\&artifactId=%s\&cves=%s" % (
         vid, gid, aid, ','.join(cves))
     url = server + path
     #date = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-    with open(filename, 'rb') as archive:
-        #md5sums = [md5(archive.read()).hexdigest()]
-        #archive.seek(0)
-        files = { 'archive': archive }
-        response = requests.put(url,
-            #headers = {'X-Victims-Api': "%s:%s" % (apiKey, generate_signature(
-            #	secret, 'PUT', path, date, md5sums)),
-            #	"Date": date},
-            files=files,
-            auth = (username, password)
-            )
-    logger.info(response.text)
+    if filename is not None:
+	with open(filename, 'rb') as archive:
+            #md5sums = [md5(archive.read()).hexdigest()]
+            #archive.seek(0)
+            files = { 'archive': archive }
+            response = requests.put(url,
+                #headers = {'X-Victims-Api': "%s:%s" % (apiKey, generate_signature(
+                #	secret, 'PUT', path, date, md5sums)),
+                #	"Date": date},
+                files=files,
+                auth = (username, password)
+                )
+        logger.info(response.text)
+    else:
+	response = requests.put(url,
+	    auth = (username, password)
+	    )
+	logger.info(response.text)
 
 logger = logging.getLogger(__name__)
