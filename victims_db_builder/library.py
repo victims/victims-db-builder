9b9c44606580128a39cce5f2093eba3eb4a4f72f
Jason Shepherd <jshepher@redhat.com>
2016-07-29 15:11:01 +1000

    getting a union of versions from both repos

--- victims_db_builder/library.py
+++ victims_db_builder/library.py
@@ -1,10 +1,12 @@
-import re
-import string
+import itertools
+import httplib, string
 import urllib2
+import re
 from version import Version
 
 class BaseLibrary(object):
     def __init__(self, versionRanges):
+        #For soup/direct maven index:
         self.versions = []
         if not isinstance(versionRanges, basestring):
             for vr in versionRanges:
@@ -12,7 +14,85 @@ class BaseLibrary(object):
         else:
             self.versions.append(Version(versionRanges))
 
+        #for Maven central
+        self.versionRanges = versionRanges
+        self.fixVersionRange()
+        #TODO push out to config
+        self.maxRange = 99
 
+    def splitRange(self, numRange):
+        tmpVers = []
+        if(',' in numRange):
+            tmpVers =  string.split(numRange, ',')
+        else:
+            tmpBase = self.retlowHigh(numRange)
+            tmpVers.append(numRange)
+            tmpVers.append(tmpBase[0])
+        return tmpVers
+
+    # Assumes string "4.0.2"
+    # Returns list of [4.0,2] for looping as float
+    def retlowHigh(self, string):
+        valList = []
+        k = string.rfind(".")
+        valList.append(string[:k])
+        valList.append(string[k+1:])
+        return valList
+
+    ## Turn version into list, without beginning symbols
+    def genVerString(self, version):
+        numRangeArray = self.splitRange(version[2:])
+        toScale = numRangeArray[0].count('.')
+        fromScale = numRangeArray[1].count('.')
+        fromValue = numRangeArray[1]
+        while fromScale < toScale:
+            fromValue += '.0'
+            fromScale = fromValue.count('.')
+            numRangeArray[1] = fromValue
+        return numRangeArray
+
+    # Remove duplicates and merge ranges in the version listing
+    def fixVersionRange(self):
+        tmpVerRanges = []
+        if (len(self.versionRanges) > 1):
+            for a, b in itertools.combinations(self.versionRanges,2):
+                AListSplit = self.genVerString(a)
+                AList = string.split(AListSplit[0], '.')
+                BListSplit = self.genVerString(b)
+                BList = string.split(BListSplit[0], '.')
+                if len(BList) != 3 or len(AList) != 3:
+                    #give up trying to fix version ranges
+                    return
+                AX = AList[0]; AY=AList[1]; AZ=AList[2]
+                BX = BList[0]; BY=BList[1]; BZ=BList[2]
+
+                if ((AX != BX) or (AY != BY)):
+                    if(not a in tmpVerRanges): tmpVerRanges.append(a)
+                    if(not b in tmpVerRanges): tmpVerRanges.append(b)
+                else:
+                    if(a[0] == b[0]):
+                        # if the same symbol for the same X.Y version,
+                        # assume last entry is correct
+                        # switch out old for new version
+                        if(not b in tmpVerRanges): tmpVerRanges.append(b)
+                    else:
+                        # symbols are different, need to combine two versions
+                        if(a[0] == '<' and  b[0] == '>'):
+                            if(AZ < BZ):
+                                if(not a in tmpVerRanges): tmpVerRanges.append(a)
+                                if(not b in tmpVerRanges): tmpVerRanges.append(b)
+                            else:
+                                tmpVersion = '<=' + str(AListSplit[0]) + ',' + BListSplit[0]
+                                tmpVerRanges.append(tmpVersion)
+                        else:
+                            if(AZ > BZ):
+                                if(not a in tmpVerRanges): tmpVerRanges.append(a)
+                                if(not b in tmpVerRanges): tmpVerRanges.append(b)
+                            else:
+                                tmpVersion = '<=' + str(BListSplit[0]) + ',' + AListSplit[0]
+                                tmpVerRanges.append(tmpVersion)
+            self.versionRanges = tmpVerRanges
+import re
 import logging
 import ConfigParser
 from bs4 import BeautifulSoup
@@ -23,18 +103,104 @@ class JavaLibrary(BaseLibrary):
         super(JavaLibrary, self).__init__(versionRange)
         self.groupId = groupId
         self.artifactId = artifactId
-        self.configure()
         self.mavenVersions = set()
-        self.confirmVersions()
+        self.configure()
 
     def configure(self):
+        config = ConfigParser.ConfigParser()
+        config.read('victims-db-builder.cfg')
+        repos = config.items('java_repos')
+        for repo, url in repos:
+            self.logger.debug('repo: %s' % repo)
+            if repo == 'central':
+                self.logger.debug('setting index to %s' % url)
+                self.indexBaseUrl = url
+                self.anchor = config.get('java', 'anchor', 1,
+                    {'groupId': self.groupId, 'artifactId' : self.artifactId})
+                self.confirmCentralVersions()
+            elif repo == 'redhat':
+                self.indexBaseUrl = url
+                self.confirmVersions()
+
+    def confirmCentralVersions(self):
+        coords = self.indexBaseUrl + self.groupId + "/" + self.artifactId
+        self.logger.debug("coords %s", coords)
         try:
-            self.indexBaseUrl = environ['VICTIMS_MAVEN_INDEX']
-        except KeyError:
-            self.logger.debug("Failed to get Maven Index from environment")
-            config = ConfigParser.ConfigParser()
-            config.read('victims-db-builder.cfg')
-            self.indexBaseUrl = config.get('java', 'index')
+            response = urllib2.urlopen(coords)
+        except urllib2.URLError, e:
+            if not hasattr(e, "code"):
+                raise
+            response = e
+            self.logger.error("Response code:%s, Error with MavenPage: %s", response.code,
+                response.msg)
+            return []
+
+        #TODO cache page locally for redundency
+        HTMLPage = response.read()
+
+        for r in self.versionRanges:
+            listString = self.genVerString(r)
+            self.logger.debug('listString %s', listString)
+
+            try:
+                #split out values, for ['9.2.8', '9.2.0']
+                valList= self.retlowHigh(listString[0])
+                firstY = float(valList[0])
+                firstZ = int(valList[1])
+                valList= self.retlowHigh(listString[1])
+                secondY = float(valList[0])
+                secondZ = int(valList[1])
+
+                if (r[0] == '>'):
+                    yMax = secondY + float('.' + str(self.maxRange))
+                    self.sortedAddVer(HTMLPage, coords, yMax, secondZ, firstY, firstZ)
+                elif (r[0] == '<'):
+                    self.sortedAddVer(HTMLPage, coords, firstY, firstZ, secondY, secondZ)
+                else:
+                    self.sortedAddVer(HTMLPage, coords, firstY, firstZ, firstY, firstZ)
+            except ValueError:
+                self.logger.debug('Couldnt generate versions for range: %s' % r)
+
+
+    def sortedAddVer(self, HTMLPage, coords, AY, AZ, BY, BZ):
+        self.logger.debug('AY:%.2f, AZ:%d, BY:%.1f, BZ:%d', AY, AZ, BY, BZ)
+        ver = BY
+        while ver >= BY and ver <= AY:
+            if (ver == BY and ver == AY):
+                for i in range (BZ,AZ+1):
+                    self.addVer(ver, i, HTMLPage, coords)
+            elif (ver == BY):
+                for i in range (BZ, self.maxRange):
+                    self.addVer(ver, i, HTMLPage, coords)
+            elif (ver == AY):
+                for i in range(AZ+1):
+                    self.addVer(ver, i, HTMLPage, coords)
+            else:
+                for i in range(self.maxRange):
+                    self.addVer(ver, i, HTMLPage, coords)
+            ver += 0.1
+
+    def addVer(self, ver, i, HTMLPage, coords):
+        tmpVers = str(ver) + "." + str(i)
+        tmpAnchor = self.anchor + tmpVers
+        results = self.regex_search(tmpVers, HTMLPage)
+        if len(results) > 0:
+            for (fullVer) in results:
+                self.mavenVersions.add(fullVer)
+        else:
+           self.logger.debug(tmpAnchor + " not found on " + coords)
+
+    def regex_search(self, tmpVers, target):
+        self.logger.debug('tmpVers: %s', tmpVers)
+        searchString = self.artifactId + '/' + tmpVers
+        searchString = searchString.replace('.', '\\.')
+        searchString += '([^"/]+)?'
+        self.logger.debug('search with regex: %s', searchString)
+        uniqueResults = set()
+        for result in re.findall(searchString, target):
+            self.logger.debug('result: %s', result)
+            uniqueResults.add("%s%s" % (tmpVers, result))
+        return uniqueResults
 
     def confirmVersions(self):
         #TODO for each index page in indexBaseUrl list
